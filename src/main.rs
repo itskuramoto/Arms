42481e53d137abe2ebd522e800ca6b297fd75d35
lion0024 <lion0024.xyz@gmail.com>
2022-04-13 16:40:36 +0900

    -n [改行なし]を追加

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ use std::env;
 
 fn main() {
     let mut str = "".to_string();
+    let mut flg: bool = false;
+
     for argument in env::args().skip(1) {
+        if argument == "-n" {
+            flg = true;
+            continue;
+        }
         str.push_str(&argument);
         str.push_str(" ");
     }
-    println!("{}", str.trim());
+
+    if flg {
+        print!("{}", str.trim());
+    } else {
+        println!("{}", str.trim());
+    }
 }
