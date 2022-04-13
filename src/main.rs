223975d0cf1d0d60995276b9e7dd6ca325c6374a
itskuramoto <itskuramoto@gmail.com>
2022-04-13 23:41:01 +0900

    add read_file_contents test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+pub fn read_file_contents(filename: String) -> String {
+    let contents = String::new();
+    contents
+}
 fn main() {
-    println!("Hello, world! from sandbox");
+    println!("Hello, world!");
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_read_file_contents() {
+        let contents = read_file_contents("text.txt".to_string());
+        assert_eq!(contents, "from text.txt");
+    }
 }
