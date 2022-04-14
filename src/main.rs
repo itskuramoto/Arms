1e55d04772ff20d8263ae2d091ace87c3b7dd6bd
lion0024 <lion0024.xyz@gmail.com>
2022-04-14 16:28:45 +0900

    コマンドライン引数の解析にclapを追加

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::env;
+use clap::{App, Arg};
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::Read;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let matches = App::new("MyApp")
+        .version("0.1")
+        .author("lion0024 <lion0024@github.com>")
+        .about("Does awesome things")
+        .arg(
+            Arg::new("output")
+                .index(1),
+        )   
+        .get_matches();
+    
+    if let Some(path) = matches.value_of("output") {
+        let mut f = File::open(path).expect("file not found");
 
-    let mut f = File::open(filename).expect("file not found");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)
+            .expect("something went wrong reading the file");
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("{}", contents);
+        println!("{}", contents);
+    } else {
+        println!("No file is specified");
+    }   
 }
