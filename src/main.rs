7fb445d42aa131d87f5abb167ba6656a9490f20a
lion0024 <lion0024.xyz@gmail.com>
2022-04-14 23:28:34 +0900

    clapをdriveベースのパースに変更

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
-use clap::{App, Arg};
 use std::fs::File;
 use std::io::Read;
+use clap::Parser;
 
 fn main() {
-    let matches = App::new("MyApp")
-        .version("0.1")
-        .author("lion0024 <lion0024@github.com>")
-        .about("Does awesome things")
-        .arg(
-            Arg::new("output")
-                .index(1),
-        )   
-        .get_matches();
-    
-    if let Some(path) = matches.value_of("output") {
-        let mut f = File::open(path).expect("file not found");
+    let args = Args::parse(); 
+    let mut f = File::open(args.file).expect("file not found");
 
-        let mut contents = String::new();
-        f.read_to_string(&mut contents)
-            .expect("something went wrong reading the file");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
 
-        println!("{}", contents);
-    } else {
-        println!("No file is specified");
-    }   
+    println!("{}", contents);
 }
+
+#[derive(Debug, Parser)]
+#[clap(name = "struct", author, about, version)]
+
+struct Args {
+    /// file path
+    file: String,
+}
+
