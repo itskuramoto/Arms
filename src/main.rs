79884da6c46f026189de2a7217b6a20f35e1723c
lion0024 <lion0024.xyz@gmail.com>
2022-04-15 22:02:04 +0900

    コマンドライン引数の解析にclapを追加

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
-use std::env;
-use std::io::{Error, Write};
+use std::io::{Error, Write, Read};
 use std::fs::File;
-use std::io::Read;
+use clap::Parser;
 
-fn file_copy(_fname: &String, _fcopy: &String) -> Result<(), Error> {
-    let mut file = File::open(_fname).expect("file not found");
+fn file_copy(_source: &String, _dest: &String) -> Result<(), Error> {
+    let mut file = File::open(_source).expect("file not found");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-    let mut f = File::create(_fcopy)?;
+    let mut f = File::create(_dest)?;
     f.write_all(contents.as_bytes())?;
     Ok(())
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    match file_copy(&args[1], &args[2]) {
+    let args = Args::parse();
+    match file_copy(&args.source, &args.dest) {
         Ok(()) => {
-            // println!("OK:file_create ");
         }
         Err(e) => {
             println!("Could not copy. Error:{}", e);
         }
     }
+}
+
+#[derive(Debug, Parser)]
+#[clap(name = "struct", author, about, version)]
+
+struct Args {
+    /// source file
+    source: String,
+
+    /// dest file
+    dest: String,
 }
