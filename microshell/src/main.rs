e961e6ae7b01020e4e7e0aa25e37ee2ab7bfd62b
lion0024 <lion0024.xyz@gmail.com>
2022-04-17 20:37:43 +0900

    forkしてプログラムが終了しないように変更

--- microshell/src/main.rs
+++ microshell/src/main.rs
@@ -1,11 +1,53 @@
-use nix::unistd::execvp;
-use std::env;
+use nix::sys::wait::*;
+use nix::unistd::{execvp, fork, ForkResult};
 use std::ffi::CString;
+use std::io::{stdin, stdout, Write};
 
 fn main() {
-    let mut cmd = Vec::new();
-    for argument in env::args().skip(1) {
-        cmd.push(CString::new(argument.to_string()).unwrap());
+    loop {
+        let line = read_line();
+        if !&line.is_empty() {
+            exec_cmd(&line);
+        }
     }
-    execvp(&cmd[0], &cmd).unwrap();
+}
+
+fn read_line() -> String {
+    print!("$ ");
+    stdout().flush().unwrap();
+
+    let mut result = String::new();
+    match stdin().read_line(&mut result) {
+        Ok(size) => {
+            if size == 0 {
+                return "".to_string();
+            } else {
+                let result = result.trim_end();
+                return result.to_string();
+            }
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            return "".to_string();
+        }
+    }
+}
+
+fn exec_cmd(_line: &str) {
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child, .. }) => {
+            waitpid(child, None).unwrap();
+        }
+        Ok(ForkResult::Child) => {
+            let lines = _line.split(' ').map(|s| s.to_string()).collect::<Vec<_>>();
+            let mut cmd = Vec::new();
+            for argument in lines {
+                cmd.push(CString::new(argument.to_string()).unwrap());
+            }
+            execvp(&cmd[0], &cmd).unwrap();
+        }
+        Err(e) => {
+            eprintln!("fork error: {}", e);
+        }
+    }
 }
