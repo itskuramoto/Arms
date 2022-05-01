d7f505a606062ac5a35a5fcb33ec21bcc0d4f784
lion0024 <lion0024.xyz@gmail.com>
2022-05-01 12:41:26 +0900

    リダイレクト機能の追加

--- microshell/src/process/builtin_cmd.rs
+++ microshell/src/process/builtin_cmd.rs
@@ -1,18 +1,51 @@
 use std::{env, path::Path};
-use std::ffi::CString;
+use nix::fcntl::{open, OFlag};
+use nix::sys::stat::Mode;
+use nix::unistd::*;
 
-pub fn run_cd(_dir: String) {
-    let path = Path::new(&_dir);
+pub fn run_cd(dir: &str) {
+    let path = Path::new(dir);
     match env::set_current_dir(path) {
         Ok(()) => {}
         Err(e) => { eprintln!("{}", e) }
     }
 }
 
-pub fn redirect(_cmd: Vec<CString>, _dir: String) {
-
+pub fn redirect(path: &str) {
+    let mode = Mode::S_IRWXU;
+    let flag = OFlag::O_CREAT | OFlag::O_WRONLY | OFlag::O_TRUNC;
+    match open_file(path, flag, mode) {
+        Ok(()) => {}
+        Err(e) => { eprintln!("{}", e) }
+    }
 }
 
-pub fn over_redirect(_cmd: Vec<CString>, _dir: String) {
-    let _path = Path::new(&_dir);
+pub fn over_redirect(path: &str) {
+    let mode = Mode::S_IRWXU;
+    let flag = OFlag::O_CREAT | OFlag::O_WRONLY | OFlag::O_APPEND;
+    match open_file(path, flag, mode) {
+        Ok(()) => {}
+        Err(e) => { eprintln!("{}", e) }
+    }
 }
+
+
+fn open_file(path: &str, flag: OFlag, mode: Mode) -> Result<(), String> {
+    match open(path, flag, mode) {
+      Ok(file) => match dup2(file, 1) {
+        Ok(_) => match close(file) {
+          Ok(_) => {}
+          Err(_) => {
+            return Err(format!("file clse error"));
+          }
+        },
+        Err(_) => {
+          return Err(format!("file dup2 error"));
+        }
+      },
+      Err(_) => {
+        return Err(format!("file open error"));
+      }
+    }
+    return Ok(());
+  }
--- microshell/src/process/parse.rs
+++ microshell/src/process/parse.rs
@@ -1,45 +1,35 @@
 use std::{ffi::CString, process::exit};
 use super::builtin_cmd;
 
-pub fn cmd_parse(_line: &str) -> Option<Vec<CString>> {
+pub fn cmd_parse(_line: &str) -> Vec<CString> {
     let lines = _line.split(' ')
         .map(|s| s.to_string())
         .collect::<Vec<_>>();
     let mut cmd = Vec::new();
-    match get_iter_next(lines.clone()) {
-        Some(iter_next) => {
-            for argument in lines.iter() {
-                if check_builtin_cmd(argument, iter_next.clone(), cmd.clone()) {
-                    return None;
-                }
-                else
-                {
-                    cmd.push(CString::new(argument.to_string()).unwrap());
-                }
-            }
-            return Some(cmd);
+    let mut cnt = 0;
+    loop {
+        if lines.len() <= cnt {
+            break;
         }
-        None => { return None; }
+        match &*lines[cnt] { 
+            ">>"    => { cnt += 1; builtin_cmd::over_redirect(&lines[cnt]) }
+            ">"     => { cnt += 1; builtin_cmd::redirect(&lines[cnt]) }
+            _       => { cmd.push(CString::new(lines[cnt].to_string()).unwrap()) }
+        }
+        cnt += 1;
     }
+    cmd
 }
 
-fn get_iter_next(lines: Vec<String>) -> Option<String> {
-    let iter_next;
-    match lines.iter().next() {
-        Some(args) => { iter_next = args }
-        None => { return None; }
+pub fn judge_builtin_cmd(_line: &str) -> bool {
+    let lines = _line.split(' ')
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>();
+    match &*lines[0] { 
+        "exit"  => { exit(0) }
+        "cd"    => { builtin_cmd::run_cd(&lines[1]) }
+        _       => { return false } 
     }
-    Some(iter_next.to_string())
+    return true;
 }
 
-fn check_builtin_cmd(argument: &str, iter_next: String, cmd: Vec<CString>) -> bool {
-    let mut is_ok = true;
-    match argument {
-        "exit"  => { exit(0) } 
-        "cd"    => { builtin_cmd::run_cd(iter_next) }
-        ">>"    => { builtin_cmd::over_redirect(cmd, iter_next) }
-        ">"     => { builtin_cmd::redirect(cmd, iter_next) }
-        _       => { is_ok = false }
-    }
-    is_ok
-} 
